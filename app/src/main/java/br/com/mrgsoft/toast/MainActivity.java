b1c0c5f9f54565d1ff71fd955afab604182c1115
Paulo Rabelo <paulorabelo@hotmail.com>
2022-02-14 22:26:37 -0300

    iniciado projeto.

---
+++ app/src/main/java/br/com/mrgsoft/toast/MainActivity.java
@@ -0,0 +1,23 @@
+package br.com.mrgsoft.toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void abrirToast(View view){
+
+        Toast.makeText(, "", Toast.LENGTH_SHORT).show();
+        
+    }
+
+}
