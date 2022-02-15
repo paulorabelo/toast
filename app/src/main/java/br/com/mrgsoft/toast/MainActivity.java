b3c8c36de435d61b65b2bf8583b9cd92c403c717
Paulo Rabelo <paulorabelo@hotmail.com>
2022-02-14 23:04:51 -0300

    Criado método Toast custom.

--- app/src/main/java/br/com/mrgsoft/toast/MainActivity.java
+++ app/src/main/java/br/com/mrgsoft/toast/MainActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -16,11 +18,27 @@ public class MainActivity extends AppCompatActivity {
 
     public void abrirToast(View view){
 
+        //Criando um Custom Toast
+        ImageView imagem = new ImageView( getApplicationContext() );
+        imagem.setImageResource( android.R.drawable.star_big_off );//Usando uma imagem
+
+        TextView textView = new TextView( getApplicationContext() );
+        textView.setBackgroundResource(R.color.teal_200);//Mudando a cor de fundo --> como mudar a cor da fonte?
+        textView.setText("Olá Toast");//Usando um texto
+
+        Toast toast = new Toast( getApplicationContext() );
+        toast.setDuration( Toast.LENGTH_LONG );
+        //toast.setView( imagem );
+        toast.setView( textView );
+        toast.show();
+
+        /*
         Toast.makeText(
                 getApplicationContext(),
                 "Ação realizada com sucesso!",
                 Toast.LENGTH_LONG
         ).show();
+         */
         
     }
 
